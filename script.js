5d68a5d14114f618335304952952e586ea4e2896
pilu9147 <119696172+pilu9147@users.noreply.github.com>
2023-07-01 19:52:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let signupBtn = document.getElementById("signup-btn");
+signupBtn.addEventListener('click',()=>{
+    window.location.href = 'http://127.0.0.1:5500/signup/index.html'
+})
+let loginBtn = document.getElementById("login-btn");
+loginBtn.addEventListener('click',()=>{
+    window.location.href = 'http://127.0.0.1:5500/login/index.html'
+})
